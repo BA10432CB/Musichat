fe322cc59e6a46cf51cafe72fb2899697f9a3a8d
BA10432CB <129589876+BA10432CB@users.noreply.github.com>
2023-07-09 12:20:23 +0000

    [edit]layouts

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,21 @@ class User < ApplicationRecord
     super && (is_active == true)
   end
 
-  has_many :posts, dependent: :destroy
+  has_many :posts,         dependent: :destroy
+  has_many :post_comments, dependent: :destroy
+  has_many :favorites,     dependent: :destroy
+
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers,  through: :reverse_of_relationships, source: :follower
+  has_many :relationships,            class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followings, through: :relationships,            source: :followed
 
   has_one_attached :profile_image
 
   validates :name,         presence: true
   validates :phone_number, presence: true
+
+  def get_profile_image
+    (profile_image.attached?) ? profile_image : 'no_image.png'
+  end
 end
